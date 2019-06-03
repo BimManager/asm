e35f49c85f38a2aacd8ba1802565545dbcec0353
Kirill Kozlov <kkozlov@10.0.2.15>
2019-06-03 03:00:51 -0700

    extended assembly format

--- playground/asm.c
+++ playground/asm.c
@@ -5,14 +5,35 @@
 
 char	*msg = "Hello, world\n";
 
+/*  inline assembly language code */
+/* __asm__ [__volatile__] ("asm code"); */
+
+/* extended assembly language code */
+/* __asm__ [__volatile__] ( */
+/* "asm code" : */
+/* output locations : */
+/* input operands : */
+/* changed registers ) */
+/* "constraints"(variable) */
+/* To reference registers use %%rax syntax */
+
 int	main(void)
 {
-  asm(
-      "\n\tmovq	$0x2000004, %rax"
-      "\n\tmovq	$0x1, %rdi"
-      "\n\tmovq _msg(%rip), %rsi"
-      "\n\tmovq	$0x10, %rdx"
-      "\n\tsyscall"
+  int	rnd;
+  
+  __asm__ __volatile__ (
+      "movq	$0x2000004, %rax \n\t"
+      "movq	$0x1, %rdi \n\t"
+      "movq	_msg(%rip), %rsi \n\t"
+      "movq	$0x10, %rdx \n\t"
+      "syscall \n\t"
       );
+
+  rnd = 42;
+  __asm__ __volatile__ (
+	"movq	%%rcx, %%rax \n\t" :
+	"=a"(rnd) :
+	"c"(rnd)
+	);
   return (0);
 }
